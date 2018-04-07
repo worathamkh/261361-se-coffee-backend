19d5533d536d0bb95ddab0ecb721e6286103d7d0
Woratham Khangtragool <worathamkh@gmail.com>
2018-04-07 16:12:48 +0700

    add /api/item/top/:n

--- routes/item.js
+++ routes/item.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('underscore');
 
 router.get('/all', (req, res) => {
 	req.models.item.find({}, { autoFetch: true }, (err, items) => {
@@ -12,6 +13,13 @@ router.get('/get/:id', (req, res) => {
 	req.models.item.find({ id: req.params.id }, { autoFetch: true }, (err, item) => {
 		if (err) throw err;
 		res.json(item);
+	});
+});
+
+router.get('/top/:n', (req, res) => {
+	req.models.item.find({}, { autoFetch: true }, (err, items) => {
+		if (err) throw err;
+		res.json(_.shuffle(items).slice(0, req.params.n));
 	});
 });
 
@@ -48,28 +56,28 @@ router.post('/new', (req, res) => {
 //
 
 router.get('/clear', (req, res) => {
-    if (req.query.magicword !== '123') {
-        res.json({ success: false });
-    } else {
-        req.models.item.find({}).remove((err) => {
-            if (err) throw err;
-            res.json({ success: true });
-        });
-    }
+  if (req.query.magicword !== '123') {
+    res.json({ success: false });
+  } else {
+    req.models.item.find({}).remove((err) => {
+      if (err) throw err;
+      res.json({ success: true });
+    });
+  }
 });
 
 router.get('/resync', (req, res) => {
-    if (req.query.magicword !== '123') {
-        res.json({ success: false });
-    } else {
-        req.models.item.drop((err) => {
-            if (err) throw err;
-            req.models.item.sync((err) => {
-                if (err) throw err;
-                res.json({ success: true });
-            });
-        });
-    }
+  if (req.query.magicword !== '123') {
+    res.json({ success: false });
+  } else {
+    req.models.item.drop((err) => {
+      if (err) throw err;
+      req.models.item.sync((err) => {
+        if (err) throw err;
+        res.json({ success: true });
+      });
+    });
+  }
 });
 
 module.exports = router;
