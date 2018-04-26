2e27ae83ce0084f9bd553d15f75768a86826f215
Woratham Khangtragool <worathamkh@gmail.com>
2018-04-26 16:54:12 +0700

    test

--- routes/order.js
+++ routes/order.js
@@ -42,6 +42,8 @@ router.post('/create', (req, res) => {
 router.post('/status/:order/:item/:status', (req, res) => {
   req.models.order.get(req.params.order, (err, order) => {
     if (err) throw err;
+    res.json(order);
+    return;
     var target = _.findIndex(order.item, i => i.id == req.params.item);
     order.item[target].status = req.params.status;
     order.item[target].extra.status = req.params.status;
